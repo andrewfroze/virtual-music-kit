d228353692ed5fd19beaf5305d9ed5e3a0719a62
Andrei Nikiporchyk <andrewfroze@gmail.com>
2026-08-28 00:59:46 +0400

    feat: create piano simple layout

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,18 @@
 import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   {
     ignores: ["dist/", "node_modules/"],
+  },
+
+  {
+    files: ["src/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
   },
 
   js.configs.recommended,
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,56 @@
-import js from "@eslint/js";
-import globals from "globals";
+import "../styles/main.scss";
 
-export default [
-  {
-    ignores: ["dist/", "node_modules/"],
-  },
+const lightKeysCount = 7;
 
-  {
-    files: ["src/**/*.js"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
-  },
+const piano = document.createElement("div");
+piano.className = "piano";
+document.body.append(piano);
 
-  js.configs.recommended,
-];
+const controlPanel = document.createElement("div");
+controlPanel.className = "piano__control-panel";
+piano.append(controlPanel);
+
+const pianoKeysHolder = document.createElement("div");
+pianoKeysHolder.className = "piano__keys-holder";
+piano.append(pianoKeysHolder);
+
+const lightKeys = [];
+const darkKeysGroupsCounts = [2, 3];
+const darkKeys = [];
+
+const lightKeysHolder = document.createElement("div");
+lightKeysHolder.className = "piano__keys-holder__light-key-holder";
+pianoKeysHolder.append(lightKeysHolder);
+
+const darkKeysHolder = document.createElement("div");
+darkKeysHolder.className = "piano__keys-holder__dark-key-holder";
+pianoKeysHolder.append(darkKeysHolder);
+
+let currentDarkKeysGroupIndex = 0;
+let currentDarkKeyInsertedCount = 0;
+for (let i = 0; i < lightKeysCount; i += 1) {
+  const lightKey = document.createElement("div");
+  lightKey.className = "piano__keys-holder__light-key";
+  lightKeys.push(lightKey);
+  lightKeysHolder.append(lightKey);
+
+  if (i < lightKeysCount - 1) {
+    if (
+      currentDarkKeyInsertedCount <
+      darkKeysGroupsCounts[currentDarkKeysGroupIndex]
+    ) {
+      const darkKey = document.createElement("div");
+      darkKey.className = "piano_keys-holder__dark-key-holder__dark-key";
+      darkKey.style.left = `${((i + 1) / lightKeysCount) * 100}%`;
+      darkKeys.push(darkKey);
+      darkKeysHolder.append(darkKey);
+      currentDarkKeyInsertedCount += 1;
+    } else {
+      currentDarkKeyInsertedCount = 0;
+      currentDarkKeysGroupIndex += 1;
+      if (currentDarkKeysGroupIndex > darkKeysGroupsCounts.length - 1) {
+        currentDarkKeysGroupIndex = 0;
+      }
+    }
+  }
+}
